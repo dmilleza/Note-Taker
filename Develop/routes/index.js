bf7857125970bbdefa5482056f6352559826df65
dmilleza <darrenmilleza@gmail.com>
2023-03-19 23:51:51 -0700

    added application functionality

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,1 +1,65 @@
 const app = require("express").Router();
+const fs = require("fs");
+const uniqid = require("uniqid");
+// const db = require("../db/db.json");
+
+app.get("/notes", (req, res) => {
+  // Read the db.json file using readFileSync, don't run without sync.
+  fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    // Send response of json data of GET request, must be parsed and stringify later
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/notes", (req, res) => {
+  // store new notes from body with req.body and id into an object
+  const newNote = {
+    ...req.body,
+    id: uniqid(),
+  };
+
+  console.log("Post Request for new notes");
+
+  //  Read data from JSON file
+  let data = fs.readFileSync("./Develop/db/db.json", "utf8");
+
+  const dataJSON = JSON.parse(data);
+
+  // Pushed new note in notes file 'db.json'
+  dataJSON.push(newNote);
+
+  // Write notes data to 'db.json' file
+  fs.writeFile(
+    "./Develop/db/db.json",
+    JSON.stringify(dataJSON),
+    (err, text) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  console.log("Success, added a new note");
+
+  // send json data response
+  res.json(data);
+});
+
+app.delete("/notes/:id", function (req, res) {
+  fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const id = req.params.id;
+    const indexOfNote = notes.findIndex((x) => x.id === parseInt(id));
+    notes.splice(indexOfNote, 1);
+    fs.writeFile("./Develop/db/db.json", JSON.stringify(notes, "\t"), (err) => {
+      if (err) throw err;
+      return true;
+    });
+    res.json(req.body.id);
+  });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/api", api);
+app.use("/api", api);
+
+app.use(express.static(path.join(__dirname, "Develop/public")));
 
 // GET Route for homepage
 app.get("/", (req, res) =>
